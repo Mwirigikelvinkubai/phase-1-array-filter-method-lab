334a80bc192820c3e7b304997a254ff9a8b355e1
mwirigikelvinkubai <mwirigiomukombiti@gmail.com>
2025-03-10 21:20:17 +0300

    filters

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// This is a trial
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export the functions
+  module.exports = { findMatching, fuzzyMatch, matchName };
+  
